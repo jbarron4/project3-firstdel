b8720823d590566f7e1d4925a488e2c6dd5802cd
jbarron4 <jbarron4@hawk.iit.edu>
2022-11-27 16:49:21 -0600

    Zoom
    Changed zoom value from 4 to 8

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ function initMap() {
 	const chicago = { lat: 41.6922572, lng: -87.5583785 }
 	// Create new Chicago map
 	const map1 = new google.maps.Map(document.getElementById("map1"), {
-		zoom: 4,
+		zoom: 8,
 		center: chicago,
 	});
 }
@@ -13,7 +13,7 @@ function initMap() {
 	const southDeering = { lat: 41.8665069, lng: -87.6300768 }
 	// Create new South Deering map
 	const map2 = new google.maps.Map(document.getElementById("map2"), {
-		zoom: 4,
+		zoom: 8,
 		center: southDeering,
 	});
 }
