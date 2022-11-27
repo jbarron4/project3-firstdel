674be0ae30c25f1426bc000bb3af5de40ea21215
jbarron4 <jbarron4@hawk.iit.edu>
2022-11-26 23:09:43 -0600

    Map Implementation Test 2
    Trying a different way of coding in the map as the first attempt didn't work

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-let map;
+/*let map;
 
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
@@ -7,4 +7,23 @@ function initMap() {
   });
 }
 
+window.initMap = initMap;*/
+
+
+
+
+let map: google.maps.Map;
+
+function initMap(): void {
+  map = new google.maps.Map(document.getElementById("map") as HTMLElement, {
+    center: { lat: 41.8665069, lng: -87.6300768 },
+    zoom: 8,
+  });
+}
+
+declare global {
+  interface Window {
+    initMap: () => void;
+  }
+}
 window.initMap = initMap;
