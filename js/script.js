a59d498987b29a74fc619b7ee4a79fa3a28bb278
jbarron4 <jbarron4@hawk.iit.edu>
2022-11-27 00:51:27 -0600

    Updated Map API Key (And Second map)
    Added a second map of my neighborhood as well as placed my new API key. Finally I added a CSS for the second map.

--- js/script.js
+++ js/script.js
@@ -15,8 +15,15 @@ window.initMap = initMap;*/
 let map: google.maps.Map;
 
 function initMap(): void {
-  map = new google.maps.Map(document.getElementById("map") as HTMLElement, {
+  map1 = new google.maps.Map(document.getElementById("map1") as HTMLElement, {
     center: { lat: 41.8665069, lng: -87.6300768 },
+    zoom: 8,
+  });
+}
+
+function initMap(): void {
+  map2 = new google.maps.Map(document.getElementById("map2") as HTMLElement, {
+    center: { lat: 41.6922572, lng: -87.5583785 },
     zoom: 8,
   });
 }
