1ab3cba6a545a67b320c5b54c2447cb4495431fb
jbarron4 <jbarron4@hawk.iit.edu>
2022-11-27 17:06:06 -0600

    JS Map Format Change 2
    Changed map format again, only adding one map and instead placing two separate markers.

--- js/script.js
+++ js/script.js
@@ -1,11 +1,25 @@
 function initMap() {
-	// Location of Chicago
-	const chicago = { lat: 41.6922572, lng: -87.5583785 }
-	// Create new Chicago map
-	const map1 = new google.maps.Map(document.getElementById("map1"), {
-		zoom: 8,
-		center: chicago,
-	});
-}
+  var chicago = { lat: 41.6922572, lng: -87.5583785 };
+  var map = new google.maps.Map(document.getElementById("map"), {
+    center: chicago,
+	zoom: 8,
+  });
 
+  new google.maps.Marker({
+    position: new google.maps.LatLng(41.6922572, -87.5583785),
+    map,
+    label: "Chicago",
+	animation: google.maps.Animation.DROP
+  });
+  
+  new google.maps.Marker({
+    position: new google.maps.LatLng(41.8665069, -87.6300768),
+    map,
+    label: "South Deering",
+	animation: google.maps.Animation.DROP
+  });
+  
+  infowindow.open(map, chicago);
+
+}
 window.initMap = initMap;
