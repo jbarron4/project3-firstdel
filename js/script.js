aae2cc11fbcdd034d45abb2658bbbd06a6b17683
jbarron4 <jbarron4@hawk.iit.edu>
2022-11-27 18:13:25 -0600

    Map Update
    Removed extra spaces in the parenthesis, changed callback of the HTML of the map API link

--- js/script.js
+++ js/script.js
@@ -1,19 +1,19 @@
 function initMap() {
-  var chicago = { lat: 41.6922572, lng: -87.5583785 };
+  var chicago = { lat:41.69225, lng:-87.55837 };
   var map = new google.maps.Map(document.getElementById("map"), {
     center: chicago,
 	zoom: 8,
   });
 
   new google.maps.Marker({
-    position: new google.maps.LatLng(41.6922572, -87.5583785),
+    position: new google.maps.LatLng(41.69225,-87.55837),
     map,
     label: "Chicago",
 	animation: google.maps.Animation.DROP
   });
   
   new google.maps.Marker({
-    position: new google.maps.LatLng(41.8665069, -87.6300768),
+    position: new google.maps.LatLng(41.86650,-87.63007),
     map,
     label: "South Deering",
 	animation: google.maps.Animation.DROP
