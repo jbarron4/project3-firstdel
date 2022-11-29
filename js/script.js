ae0bcc694c42a4a655b72a05111da246aeb4fc68
jbarron4 <jbarron4@hawk.iit.edu>
2022-11-29 02:59:42 -0600

    Map Attempt & Media Query
    Changed margins on media query in CSS and attempted to fix the map javaScript

--- js/script.js
+++ js/script.js
@@ -1,16 +1,10 @@
-/*function initMap(): void {
-  var chicago = { lat:41.69225, lng:-87.55837 };
-  var map = new google.maps.Map(document.getElementById("map"), {
-    center: chicago,
-	zoom: 8,
-  });
-
-  new google.maps.Marker({
-    position: new google.maps.LatLng(41.69225,-87.55837),
-    map,
-    label: "Chicago",
-	animation: google.maps.Animation.DROP
-  });
+function myMap() {
+  var chicago= {
+    center:new google.maps.LatLng(41.69225,-87.55837),
+    zoom:8,
+  };
+  
+  var map = new google.maps.Map(document.getElementById("map"),chicago);
   
   new google.maps.Marker({
     position: new google.maps.LatLng(41.86650,-87.63007),
@@ -19,15 +13,7 @@
 	animation: google.maps.Animation.DROP
   });
   
-  infowindow.open(map, chicago);
-
+  infoWindow.open(map, chicago);
 }
-window.initMap = initMap;*/
 
-function myMap() {
-var chicago= {
-  center:new google.maps.LatLng(41.69225,-87.55837),
-  zoom:8,
-};
-var map = new google.maps.Map(document.getElementById("map"),chicago);
-}
+window.myMap = myMap;
