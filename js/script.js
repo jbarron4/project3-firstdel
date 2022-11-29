4f8137ac1593844794ac2bd987004f09b93f5d92
jbarron4 <jbarron4@hawk.iit.edu>
2022-11-29 17:34:53 -0600

    Map Attempt
    Trying to fix the map based on an online source I found

--- js/script.js
+++ js/script.js
@@ -1,17 +1,15 @@
-function myMap() {
-  var chicago= {
-    center:new google.maps.LatLng(41.69225,-87.55837),
-    zoom:8,
-  };
-  
-  var map = new google.maps.Map(document.getElementById("map"),chicago);
-  
+function initMap() {
+  var chicago = { lat:41.8781, lng:-87.6298};
+  var map = new google.maps.Map(document.getElementById("map"), {
+    center: chicago,
+	zoom: 8,
+  });
+
   new google.maps.Marker({
     position: new google.maps.LatLng(41.86650,-87.63007),
     map,
     label: "South Deering",
 	animation: google.maps.Animation.DROP
   });
-  
-  infoWindow.open(map, chicago);
 }
+window.initMap = initMap;
