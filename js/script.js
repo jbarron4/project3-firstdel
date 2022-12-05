49ac29cbee82aa9c77d11c1b8f5f98f887a891f3
jbarron4 <jbarron4@hawk.iit.edu>
2022-12-05 17:14:31 -0600

    Div into Section
    Placed the div into the section for the maps instead of just in the content

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 function initMap() {
 	var map_parameters = {center: {lat:41.8781, lng:-87.6298}, zoom: 8};
-	var map = new google.maps.Map(document.getElementById("map"), map_parameters);
+	var map = new google.maps.Map(document.getElementById('map'), map_parameters);
 	
 	var chicago = {position: {lat:41.8781, lng:-87.6298}, map: map};
 	var southDeering = {position: {lat: 41.86650, lng: -87.63007}, map: map};
