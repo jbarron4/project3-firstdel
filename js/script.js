7ef6547aee3ddbb32d612ded9684026e685baabf
jbarron4 <jbarron4@hawk.iit.edu>
2022-12-05 16:50:01 -0600

    Maps Script Rewrite
    Rewrote the script for maps to make is simpler

--- js/script.js
+++ js/script.js
@@ -1,24 +1,10 @@
 function initMap() {
-  var chicago = { lat:41.8781, lng:-87.6298};
-  var map = new google.maps.Map(document.getElementById('map'), {
-    center: chicago,
-	zoom: 8,
-  });
-
-  new google.maps.Marker({
-    position: new google.maps.LatLng(41.86650,-87.63007),
-    map,
-    label: "South Deering",
-	animation: google.maps.Animation.DROP
-  });
+	var map_parameters = {center: {lat:41.8781, lng:-87.6298}, zoom: 8};
+	var map = new google.maps.Map(document.getElementById('map'), map_parameters);
+	
+	var chicago = {position: {lat:41.8781, lng:-87.6298}, map: map};
+	var southDeering = {position: {lat: 41.86650, lng: -87.63007}, map: map};
+	
+	var marker1 = new google.maps.Marker(chicago);
+	var marker1 = new google.maps.Marker(southDeering);
 }
-
-window.initMap = initMap;
-
-
-
-/*
-function initMap() {
-	var chicago = {center: {lat: 41.86650, lng: -87.63007}, zoom: 8}
-	var map = new google.maps.Map(document.getElementById('map')
-*/
