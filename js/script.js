8962aa38e8a43afec2cc624436308eccfead1317
jbarron4 <jbarron4@hawk.iit.edu>
2022-12-05 17:25:05 -0600

    Code Rewrite
    Rewrite JS and HTML for the map in an attempt to reset it

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
+let map;
+
 function initMap() {
-	var map_parameters = {center: {lat:41.8781, lng:-87.6298}, zoom: 8};
-	var map = new google.maps.Map(document.getElementById('map'), map_parameters);
-	
-	var chicago = {position: {lat:41.8781, lng:-87.6298}, map: map};
-	var southDeering = {position: {lat: 41.86650, lng: -87.63007}, map: map};
-	
-	var marker1 = new google.maps.Marker(chicago);
-	var marker1 = new google.maps.Marker(southDeering);
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat:41.878, lng:-87.629 },
+    zoom: 8,
+  });
 }
+
+window.initMap = initMap;
